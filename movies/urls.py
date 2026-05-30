60756d265a1c89c14969e9e1e9e76e1411dc8d76
KosNT <kosnt.mob@gmail.com>
2026-05-30 18:30:41 +0500

    feat: подключить index.html к шаблону

--- movies/urls.py
+++ movies/urls.py
@@ -3,7 +3,7 @@ from movies import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    #path('movie/<slug:movie_slug>/',views.movies, name='movie_detail'),
+    #path('movie/<slug:movie_slug>/',views.movie, name='movie_detail'),
     #path('genre/<str:genre_name>/',views.genres, name='genre_filter'),
     #path('year/<year4:year>/', views.years, name='year_filter'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+movies_db = [
+    {'id': 1, 'title': 'Интерстеллар', 'slug': 'interstellar', 'year': 2014, 'genre': 'sci-fi', 'desc': 'Путешествие сквозь черную дыру.'},
+    {'id': 2, 'title': 'Начало', 'slug': 'inception', 'year': 2010, 'genre': 'sci-fi', 'desc': 'Проникновение в чужие сны.'},
+    {'id': 3, 'title': 'Зеленая миля', 'slug': 'the-green-mile', 'year': 1999, 'genre': 'drama', 'desc': 'История в тюрьме Луизианы.'},
+    {'id': 4, 'title': 'Побег из Шоушенка', 'slug': 'shawshank', 'year': 1994, 'genre': 'drama', 'desc': 'История о надежде и дружбе.'},
+    {'id': 5, 'title': 'Джентльмены', 'slug': 'the-gentlemen', 'year': 2019, 'genre': 'comedy', 'desc': 'Криминальный мир Лондона.'},
+]
 # Create your views here.
 def index(request):
-    return render(request, 'movies/index.html')
+    return render(request, 'movies/index.html', context={'movies': movies_db})
 
